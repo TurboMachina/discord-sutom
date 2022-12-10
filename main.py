db62a933d30c9a2bab6742f0e27de6394ba54307
Antoine Herrent <herrentoine21@gmail.com>
2022-12-10 16:53:47 +0100

    Connection, message callback, template sutom obj

--- main.py
+++ main.py
@@ -3,6 +3,32 @@ import discord
 from dotenv import load_dotenv
 from datetime import datetime
 
+import results_handler as rd
+
+
+# Example of fetched message : 
+""" 
+#SUTOM #999 4/6 13:37
+
+🟥🟦🟡🟡🟡🟦🟡
+🟥🟥🟦🟡🟡🟡🟦
+🟥🟥🟡🟡🟡🟦🟡
+🟥🟥🟥🟥🟥🟥🟥
+
+https://sutom.nocle.fr
+"""
+
+class SutomTry:
+    def __init__(self, user_id, sutom_number, number_of_try, word_len, date_of_try, time_of_try, time_to_guess = '00:00:00'):
+        self.user_id = user_id
+        self.sutom_number = sutom_number
+        self.number_of_try = number_of_try
+        self.word_len = word_len
+        self.time_to_guess = time_to_guess
+        self.date_of_try = date_of_try
+        self.time_of_try = time_of_try
+
+
 def test_bot_connection(client):
     TEST_CHANNEL = os.getenv('TEST_CHANNEL_ID')
     TEST_GUILD = os.getenv('TEST_GUILD_ID')
@@ -18,21 +44,41 @@ def test_bot_connection(client):
         )
         gen_channel = guild.get_channel(int(TEST_CHANNEL))
         await gen_channel.send("TEST_PASSED_{}".format(datetime.now()))
+    @client.event
+    async def on_message(message):
+        if (message.author == client.user):
+            return
+        if (message.content[0:6] == "#SUTOM"):
+            print("DETECTED")
+
+def handle_sutom_message(sutom_message: discord.message):
+    # Parse the message
+    sutom_try = SutomTry()
+    
 
 
 def main():
     
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
-
-    client = discord.Client(intents=discord.Intents.default())
+    
+    intents = discord.Intents.all()
+    #intents.messages = True
+    #intents.message_content = True
+    client = discord.Client(intents=intents)
 
     SUTOM_CHANNEL = os.getenv('SUTOM_CHANNEL_ID')
     SUTOM_GUILD = os.getenv('SUTOM_GUILD_ID')
 
     #test_bot_connection(client)
 
-    
+    @client.event
+    async def on_message(message):
+        if (message.author == client.user):
+            return
+        if (message.content[0:6] == "#SUTOM"):
+            handle_sutom_message(message)
+
 
     client.run(TOKEN)
 
---
+++ results_handler.py
@@ -0,0 +1,17 @@
+import json
+
+def write_json(file_path, sutom_results):
+  with open(file_path, 'r') as f:
+    data = json.load(f)
+
+  data['discord_id'] = '1234567890'
+  data['results'] = [1, 2, 3]
+  data['time'] = '12:00:00'
+  data['date'] = '2022-12-10'
+
+  with open(file_path, 'w') as f:
+    json.dump(data, f)
+
+def read_results(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
